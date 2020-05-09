af84dc67ebb5a354fa3086e01e869e51dd6bb57f
hughes <hughes@tumelo.com>
2020-05-09 13:14:19 +0100

    added a user model

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const { Keystone } = require("@keystonejs/keystone");
 const { MongooseAdapter } = require("@keystonejs/adapter-mongoose");
 const { GraphQLApp } = require("@keystonejs/app-graphql");
-const { Text } = require("@keystonejs/fields");
+
+const TodoSchema = require("./lists/Todo.js");
+const UserSchema = require("./lists/User");
 
 const keystone = new Keystone({
   name: "Tutorial",
   adapter: new MongooseAdapter(),
 });
 
-keystone.createList("Todo", {
-  fields: {
-    name: {
-      type: Text,
-    },
-  },
-});
+keystone.createList("Todo", TodoSchema);
+keystone.createList("User", UserSchema);
 
 module.exports = {
   keystone,
---
+++ lists/Todo.js
@@ -0,0 +1,26 @@
+const { Text, CalendarDay, Checkbox } = require("@keystonejs/fields");
+
+module.exports = {
+  fields: {
+    description: {
+      type: Text,
+      isRequired: true,
+    },
+    isComplete: {
+      type: Checkbox,
+      defaultValue: false,
+    },
+    deadline: {
+      type: CalendarDay,
+      format: "Do MMMM YYYY",
+      yearRangeFrom: "2019",
+      yearRangeTo: "2029",
+      isRequired: false,
+      defaultValue: new Date().toISOString("YYYY-MM-DD").substring(0, 10),
+    },
+    assignee: {
+      type: Text,
+      isRequired: true,
+    },
+  },
+};
