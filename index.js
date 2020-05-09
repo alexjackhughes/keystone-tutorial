99c3f6b0c81205f8e7f819cb8505f6a80652db30
hughes <hughes@tumelo.com>
2020-05-09 13:01:06 +0100

    Initial GraphQL playground created

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { Keystone } = require("@keystonejs/keystone");
 const { MongooseAdapter } = require("@keystonejs/adapter-mongoose");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { Text } = require("@keystonejs/fields");
 
 const keystone = new Keystone({
-  name: "New Project",
+  name: "Tutorial",
   adapter: new MongooseAdapter(),
 });
+
+keystone.createList("Todo", {
+  fields: {
+    name: {
+      type: Text,
+    },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp()],
+};
